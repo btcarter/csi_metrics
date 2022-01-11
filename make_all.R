a1a9964b95f64652bfd9e199ffd375d3f837e093
btcarter <benjamin.t.carter@gmail.com>
2022-01-10 17:02:51 -0700

    lazy commit

--- make_all.R
+++ make_all.R
@@ -34,30 +34,32 @@ source(file = file.path(SCRIPT_DIR,
                         "preprocessing.R"))
 
 # DEPARTMENT PUBLCATION CV ####
-rmarkdown::render(
-  input = file.path(SCRIPT_DIR,
-                    "report_pubs_list.Rmd"),
-  output_format = c("html_document",
-                    "word_document",
-                    "md_document"),
-  output_dir = OUT_DIR,
-  output_file = c("dept_pubs.html",
-                  "dept_pubs.docx",
-                  "dept_pubs.md")
-)
-
-# DEPARTMENT REPORT ####
 # rmarkdown::render(
 #   input = file.path(SCRIPT_DIR,
-#                     "report_department.Rmd"),
-#   output_format = c("html_document" 
-#                     ,"powerpoint_presentation"
-#   ),
+#                     "report_pubs_list.Rmd"),
+#   output_format = c("html_document",
+#                     "word_document",
+#                     "md_document"),
 #   output_dir = OUT_DIR,
-#   output_file = c("general_report1"
-#                   ,"general_report1"
-#   )
+#   output_file = c("dept_pubs.html",
+#                   "dept_pubs.docx",
+#                   "dept_pubs.md")
 # )
+
+# DEPARTMENT REPORT ####
+rmarkdown::render(
+  input = file.path(SCRIPT_DIR,
+                    "report_department.Rmd"),
+  output_format = c("html_document"
+                    ,"slidy_presentation"
+                    # ,"powerpoint_presentation"
+  ),
+  output_dir = OUT_DIR,
+  output_file = c("general_report1"
+                  ,"general_report1_slides"
+                  # ,"general_report1_slides"
+  )
+)
 
 # CREATE INVESTIGATOR REPORTS
 
--- preprocessing.R
+++ preprocessing.R
@@ -61,20 +61,25 @@ df_studies$study_stage <- factor(df_studies$study_stage,
                                             "Submitted",
                                             "Accepted"))
 
+df_dissem$target_audience <- factor(df_dissem$target_audience,
+                                    ordered = TRUE,
+                                    levels = c("Organization",
+                                               "Local",
+                                               "State",
+                                               "Regional",
+                                               "USA",
+                                               "International",
+                                               "Not USA"))
 
-labels_df_manuscripts <- c("Authors",
-                           "Type of Publication",
-                           "Title",
-                           "Publication/Meeting",
-                           "Date of Publication/Presentation (per Pub Med)",
-                           "Pubmed ID",
-                           "Google Scholar ID",
-                           "PI Google Scholar ID",
-                           "Include",
-                           "URL",
-                           "DOI")
+df_dissem$csi_support <- factor(df_dissem$csi_support,
+                                    ordered = TRUE,
+                                    levels = c("None",
+                                               "Editing or submission",
+                                               "Regulatory",
+                                               "Acknowledgements",
+                                               "Authorship"))
 
-# GET NCBI DATA via iCite API
+# GET NCBI DATA via iCite API ##################################################
 # https://icite.od.nih.gov/api
 # journal subset
 pmids <- unique(df_dissem$pmid)
@@ -85,18 +90,53 @@ q <- paste("https://icite.od.nih.gov/api/pubs?pmids=",
            sep = "",
            q)
 
+# q <- paste("https://icite.od.nih.gov/api/pubs?pmids=",
+#            sep = "",
+#            34706876)
+
 res <- GET(q)
 
 # rawToChar(res$content)
 man_meta <- fromJSON(rawToChar(res$content))
 df_man_meta <- man_meta$data
 
+df_man_meta$pmid <- as.character(df_man_meta$pmid)
+
+# GET DATA ON ARTICLES THAT CITE US ############################################
+df_cited_by <- df_man_meta %>% 
+  select(
+    manuscript_pmid = pmid,
+    cited_by_pmid = cited_by
+  ) %>% 
+  unnest(
+    cited_by_pmid
+  ) %>% 
+  mutate(
+    cited_by_pmid = as.character(cited_by_pmid)
+  )
+
+pmids <- unique(df_cited_by$cited_by_pmid)
+q <- paste(pmids, collapse = ",")
+q <- paste("https://icite.od.nih.gov/api/pubs?pmids=",
+           sep = "",
+           q)
+
+res <- GET(q)
+
+man_meta <- fromJSON(rawToChar(res$content))
+df_cited_by_meta <- man_meta$data
+
+df_cited_by_meta$pmid <- as.character(df_cited_by_meta$pmid)
+
+
+# tmp <- strsplit(df_man_meta$authors, split = ", ")
+# cat(paste(tmp[[1]], collapse="\n"))
 
 # Other potential sources
 # https://www.ncbi.nlm.nih.gov/pmc/tools/get-metadata/
 # https://www.ncbi.nlm.nih.gov/pmc/tools/developers/
 
-# AUTHOR GOOGLE SCHOLAR DATA
+# AUTHOR GOOGLE SCHOLAR DATA ##################################################
 # get author citation history
 # get_gs_stats <- function(x){
 #   
@@ -133,7 +173,7 @@ df_man_meta <- man_meta$data
 #                    "Users",
 #                    "CarteB",
 #                    "OneDrive - BILLINGS CLINIC",
-#                    "goals.xlsx")
+#                    "worklog.xlsx")
 # 
 # df_ts <- read_xlsx(fl_nm,
 #                    sheet = "timeTrackerData")
@@ -141,11 +181,11 @@ df_man_meta <- man_meta$data
 # df_ts_studies <- read_xlsx(fl_nm,
 #                            sheet = "project_list")
 # 
-# df_ts <- df_ts %>% 
+# df_ts <- df_ts %>%
 #   mutate(
 #     time = as.numeric(dts_out - dts_in)
-#   ) %>% 
+#   ) %>%
 #   mutate(
-#     fte_time = time/(40*60)
+#     hrs = time/(60)
 #   )
 
